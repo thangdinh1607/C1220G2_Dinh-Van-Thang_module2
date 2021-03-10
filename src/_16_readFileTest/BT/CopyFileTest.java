aa301f34831a8e5d83824dc6e046dbf073a3b02f
thangdinh1607 <thangdinh1607@gmail.com>
2021-03-10 09:56:14 +0700

    up

--- src/_16_readFileTest/BT/CopyFileTest.java
+++ src/_16_readFileTest/BT/CopyFileTest.java
@@ -9,46 +9,35 @@ public class CopyFileTest {
 //    public static final String linkFileReader = "C:\\Users\\thang\\Desktop\\a2.txt";
 //    public static final String linkFileWrite = "C:\\Users\\thang\\Desktop\\a1.txt";
 
-    ArrayList<String> readerFile(String link) throws FileNotFoundException {
-        File file = new File(link);
+    ArrayList<String> readerFile(String linkRead, String linkWrite) throws FileNotFoundException {
+        File fileRead = new File(linkRead);
+        File fileWrite = new File(linkWrite);
         ArrayList<String> arrayList = new ArrayList<>();
 
         try {
-            FileReader fileReader = new FileReader(file);
+            FileReader fileReader = new FileReader(fileRead);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            if (!file.exists()) {
+            FileWriter fileWriter = new FileWriter(fileWrite);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            if (!fileRead.exists()) {
                 throw new FileNotFoundException();
             }
             String line;
             int i = 0;
             while ((line = bufferedReader.readLine()) != null) {
-                arrayList.add(i, line);
-                i++;
+                bufferedWriter.write(line);
             }
             bufferedReader.close();
+            bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return arrayList;
     }
 
-    void writeCVS(List arr, String link) {
-        File file = new File(link);
-        try {
-            FileWriter fileWriter = new FileWriter(file);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            if (!file.exists()) {
-                throw new FileNotFoundException();
-            }
-                
-            bufferedWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) throws FileNotFoundException {
         CopyFileTest copyFileTest = new CopyFileTest();
-        copyFileTest.writeCVS(copyFileTest.readerFile("C:\\Users\\thang\\Desktop\\a2.txt"), "C:\\Users\\thang\\Desktop\\a1.txt");
+        copyFileTest.readerFile("C:\\Users\\thang\\Desktop\\a2.txt", "C:\\Users\\thang\\Desktop\\a1.txt");
     }
 }
